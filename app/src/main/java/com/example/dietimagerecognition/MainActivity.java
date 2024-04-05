8e3a61a873f6a3de5bf688e505ce573be08c0841
ahddl <101929817+ahddl@users.noreply.github.com>
2024-04-06 01:40:51 +0900

    v.1.0.2

--- app/src/main/java/com/example/dietimagerecognition/MainActivity.java
+++ app/src/main/java/com/example/dietimagerecognition/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.dietimagerecognition;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton gallerybutton;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +36,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button camerabutton = findViewById(R.id.camerabutton);
+        ImageButton camerabutton = findViewById(R.id.camerabutton);
         camerabutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Menu.class);
                 startActivity(intent);
+            }
+        });
+
+        ImageButton gallerybutton = findViewById(R.id.gallerybutton);
+        gallerybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
             }
         });
 
@@ -34,4 +59,77 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        galleryLauncher.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        Uri fileUri = data.getData();
+                        if (fileUri != null) {
+                            ContentResolver resolver = getContentResolver();
+
+                            try {
+                                InputStream instream = resolver.openInputStream(fileUri);
+                                Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                                gallerybutton.setImageBitmap(imgBitmap);
+
+                                instream.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            });
+
+   /* @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==101){
+            if(resultCode==RESULT_OK){
+                Uri fileUri = data.getData();
+
+                ContentResolver resolver = getContentResolver();
+
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    gallerybutton.setImageBitmap(imgBitmap);
+
+                    instream.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }*/
+
 }
+
+   /* ActivityResultLauncher<Intent>startActivityResult=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                uri = result.getData().getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getContentUri(getContentResolver(), uri);
+                    gallerybutton.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });*/
