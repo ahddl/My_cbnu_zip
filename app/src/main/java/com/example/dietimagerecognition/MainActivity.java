3e20bc107b29b6be73d14cdca3834dfd3ae3db8e
ahddl <101929817+ahddl@users.noreply.github.com>
2024-04-06 03:22:09 +0900

    v.1.0.3

--- app/src/main/java/com/example/dietimagerecognition/MainActivity.java
+++ app/src/main/java/com/example/dietimagerecognition/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.dietimagerecognition;
 
+import static java.nio.file.Files.createFile;
+
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -17,10 +22,12 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton gallerybutton;
     Uri uri;
+    ImageView imageView;
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +45,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        View anotherLayout = getLayoutInflater().inflate(R.layout.activity_menu, null);
+
+        imageView = anotherLayout.findViewById(R.id.imageView);
         ImageButton camerabutton = findViewById(R.id.camerabutton);
-        camerabutton.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Menu.class);
                 startActivity(intent);
+            }
+        });
+
+        camerabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
             }
         });
 
@@ -59,6 +80,62 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void takePicture(){
+        try {
+            file = createFile();
+            if (file.exists()) {
+                file.delete();
+            }
+
+            file.createNewFile();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if(Build.VERSION.SDK_INT>=24){
+            uri= FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+        }else{
+            uri=Uri.fromFile(file);
+        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+
+        cameraLauncher.launch(intent);
+    }
+
+    private File createFile(){
+        String filename = "capture.jpg";
+        File outFile = new File(getFilesDir(),filename);
+        Log.d("Main","File path : "+ outFile.getAbsolutePath());
+
+        return outFile;
+    }
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==101 && resultCode == RESULT_OK){
+    BitmapFactory.Options options = new BitmapFactory.Options();
+    options.inSampleSize=8;
+    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options);
+
+    imageView.setImageBitmap(bitmap);
+        }
+    }*/
+
+    private final ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 8;
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+
+                    imageView.setImageBitmap(bitmap);
+                }
+            });
 
     public void openGallery() {
         Intent intent = new Intent();
@@ -90,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+}
 
    /* @Override
     protected void onActivityResult(int requestCode,int resultCode, Intent data){
@@ -114,7 +192,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }*/
 
-}
 
    /* ActivityResultLauncher<Intent>startActivityResult=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
